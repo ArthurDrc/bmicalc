3ba5dcdbdd131086e98842fe8be31e5e39902642
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2019-01-08 13:20:46 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 int main()
 {
-float weight;
-float height;
-float bmi;
+//floats
+    float weight;
+    float height;
+    float bmi;
 
-//set weight rules (>=30) + basic codes   
-while (weight < 30){
-printf("Enter your body weight in KILLOGRAM: \n");
+    printf("Enter your body weight in KILLOGRAM: \n");
     scanf("%f", &weight);
     getchar();
-printf("Please enter a vaild amount\n");
-}
-while (weight =30, weight >30){
-printf("Enter your body weight in KILLOGRAM: \n");
-    scanf("%f", &weight);
+    printf("Enter your body height in CENTIMETER: \n");
+    scanf("%f", &height);
     getchar();
-}
-//set rules of height (>=100) + basic codes
-while (height < 100) {
-printf("Enter your body height in CENTIMETER: \n");
-    scanf("%f", &height);
-getchar();
-printf("Please enter a vaild amount\n");
-}
-while (height = 100, height >100) {
-printf("Enter your body height in CENTIMETER: \n");
-    scanf("%f", &height);
-getchar();
-}
     bmi = weight*100 / (height*height)*100;
     printf("Your BMI is %.1f \n", bmi);
-return 0;
+if (bmi < 20) {
+    printf("You are underweighted\n");
+}
+if (bmi > 20, bmi <25) {
+    printf("You are normal\n");
+}
+if (bmi >25){
+    printf("You are overweighted\n");
+}
+    return 0;
 }
